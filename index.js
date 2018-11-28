3c2010fbcf210185538a3440dd081e4221834428
antongavrilov <gavranton@gmail.com>
2018-11-29 01:34:04 +0300

    main logic changes

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var extract = require('./extract');
+
+var handleError = function(err, res){
+  res.writeHead(404);
+  res.end();
+}
 
 var server = http.createServer(function(req, res){
-  fs.readFile('app/index.html', function(err, data){
-    res.end(data);
+
+  var filePath = extract(req.url);
+
+  fs.readFile(filePath, function(err, data){
+    if(err){
+      handleError(err, res);
+      console.log('error');
+      return;
+    }else{
+      res.end(data)
+    }
   })
 })
 
