8acae090e4faa2c0735b7c863ab632cad83caac4
Anton Gavrilov <anton.gavrilov@kofax.com>
2018-12-20 19:56:22 +0300

    new modules ChatList, ChatForm

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,16 +1,24 @@
 import {ret as socket}  from './ws-client';
+import {ChatForm, ChatList} from './dom';
+const FORM_SELECTOR = '[data-chat="chat-form"]';
+const INPUT_SELECTOR = '[data-chat="message-input"]'
+const LIST_SELECTOR = '[data-chat="message-list"]'
 
 class ChatApp {
   constructor() {
     socket.init('ws://localhost:3002');
+    this.chatFrom = new ChatForm(FORM_SELECTOR, INPUT_SELECTOR);
+    this.chatList = new ChatList(LIST_SELECTOR, 'wonderwoman');
     socket.registerOpenHandler(() => {
-      let message = new ChatMessage({
-        message: 'pow!'
-      });
-      socket.sendMessage(message.serialize());
-    })
+      this.chatFrom.init((data)=>{
+        let message = new ChatMessage({message: data});
+        socket.sendMessage(message.serialize());
+      })
+      })
+
     socket.registerMassageHandler(data => {
-      console.log(data);
+      let message = new ChatMessage(data);
+      this.chatList.drawMessage(message.serialize());
     })
 
     socket.registerCloserHandler(e => {
