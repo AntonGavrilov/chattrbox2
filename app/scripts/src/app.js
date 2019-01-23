ab9b1dc07ec10507e372289ec8aafb7eef218021
Anton Gavrilov <anton.gavrilov@kofax.com>
2019-01-23 20:54:42 +0300

    room list implementation

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -1,15 +1,26 @@
-import {ret as socket}  from './ws-client';
-import {UserStore, MessageStore} from './storage';
-import {ChatForm, ChatList, promptForUsername} from './dom';
+import {
+  ret as socket
+} from './ws-client';
+import {
+  UserStore,
+  MessageStore
+} from './storage';
+import {
+  ChatForm,
+  ChatList,
+  promptForUsername
+} from './dom';
 const FORM_SELECTOR = '[data-chat="chat-form"]';
 const INPUT_SELECTOR = '[data-chat="message-input"]'
 const LIST_SELECTOR = '[data-chat="message-list"]'
+const ROOMLIST_SELECTOR = '[data-chat="message-list"]'
 
 let userStore = new UserStore('x-chattrbox/u');
 let messageStore = new MessageStore('x-chattrbox/m');
 let username = userStore.get();
+let currentRoom = 'mainRoom'
 
-if(!username){
+if (!username) {
   username = promptForUsername();
   userStore.set(username)
 }
@@ -29,12 +40,14 @@ class ChatApp {
     }
 
     socket.registerOpenHandler(() => {
-      this.chatFrom.init((data)=>{
-        let message = new ChatMessage({message: data});
+      this.chatFrom.init((data) => {
+        let message = new ChatMessage({
+          message: data
+        });
         socket.sendMessage(message.serialize());
       })
 
-      })
+    })
 
     socket.registerMassageHandler(data => {
       let message = new ChatMessage(data);
@@ -60,22 +73,27 @@ class ChatMessage {
   constructor({
     message: m,
     user: u = username,
+    room: r = currentRoom,
     timestamp: t = (new Date()).getTime()
   }) {
     this.message = m;
     this.user = u;
     this.timestamp = t;
-    this.isNewMessage = true;
+    this.room = r,
+      this.isNewMessage = true;
   }
 
   serialize() {
     return {
       user: this.user,
       message: this.message,
+      room: this.room,
       timestamp: this.timestamp,
       isNewMessage: this.isNewMessage
     }
   }
 }
 
-export {ChatApp};
+export {
+  ChatApp
+};
